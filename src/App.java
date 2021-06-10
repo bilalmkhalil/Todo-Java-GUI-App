063215fe85b55c14bed59ae28a63257800b67e8a
Bilal Khalil Khankhail <bilalmkhalil255@gmail.com>
2021-06-10 22:50:27 +0500

    Actions Listeners & Others component added

--- src/App.java
+++ src/App.java
@@ -1,19 +1,39 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 class App extends JFrame {
     public App() {
+        JPanel sideBar = new JPanel();
+        sideBar.setBounds(0, 0, 300, 800);
+        sideBar.setBackground(Color.LIGHT_GRAY);
+
+        JPanel listContainer = new JPanel();
+        listContainer.setPreferredSize(new Dimension(420, 800));
+        listContainer.setBounds(430, 0, 420, 800);
+        
+
+
         inputPanel upperPanel = new inputPanel();
         mainPanel panel = new mainPanel();
 
-        add(upperPanel);
-        add(panel);
+        upperPanel.setList(panel);
+
+        listContainer.add(upperPanel);
+        listContainer.add(panel);
+        
+        add(sideBar);
+        add(listContainer);
 
         setTitle("ExON Todo");
-        setSize(1024,800);
+        setSize(1000,800);
         setLayout(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        setVisible(true);  
     }
     public static void main(String[] args) {
         new App();
--- src/mainPanel.java
+++ src/mainPanel.java
@@ -1,21 +1,61 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 
 public class mainPanel extends JPanel {
-    JList<String> list;
+    private JList<String> list;
+    private DefaultListModel<String> listModel;
+    private JPanel listPanel;
     
     public mainPanel() {
+
+        listPanel = new JPanel();
+        listPanel.setBackground(Color.WHITE);
+        listPanel.setPreferredSize(new Dimension(325, 400));
+        listPanel.setLayout(new GridLayout());
+        listPanel.setBorder(BorderFactory.createTitledBorder("TODO LIST"));
+        // listPanel.setBorder(BorderFactory.createMatteBorder(15, 15, 15, 15, Color.WHITE));
+
         String[] week = { "Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday","Sunday" };
 
         list = new JList<String>(week);
-        list.setBackground(Color.ORANGE);
-        list.setBounds(0,0, 400, 600);
+        list.setFixedCellHeight(25);
+        list.setBorder(BorderFactory.createMatteBorder(10, 15, 15, 15, Color.WHITE));
+        // list.setRequestFocusEnabled(false);
+        // list.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        // add(listPanel);
-        add(list);
-        setBackground(Color.red);
-        setBounds(0,200, 1024, 600);
+        listModel = new DefaultListModel<String>();
+        listModel.addElement(week[0]);
+        listModel.addElement(week[1]);
+        listModel.addElement(week[2]);
+        listModel.addElement(week[3]);
+
+        list.setModel(listModel);
+
+        listPanel.add(list);
+        add(listPanel);
+        setBackground(Color.WHITE);
+        setBounds(0,200, 420, 600);
+    }
+
+    public void setListValue(String text) {
+        listModel.addElement(text);
+    }
+    public int indexVal() {
+        return list.getSelectedIndex();
+    }
+    public void delListValue(int index) {
+        listModel.remove(index);
+    }
+    public void updateListValue(int index, String text) {
+        listModel.add(index, text);
+    }
+    public boolean isSelected() {
+        return list.isSelectionEmpty();
     }
 }
